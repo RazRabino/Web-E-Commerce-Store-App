096ad09a236c12215fb02ba36e8270a47ef31464
RazRabino <100515676+RazRabino@users.noreply.github.com>
2022-06-17 20:22:37 +0300

    Categories component

--- src/data.js
+++ src/data.js
@@ -23,3 +23,65 @@ export const sliderItems = [
         bg: "#F5F5DC",
     },
 ]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2020/07/24/16/37/cactus-5434469_960_720.jpg",
+        title: "SUMMER SALE",
+    },
+
+    {
+        id: 2,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/38/cactus-2556000_960_720.jpg",
+        title: "AUTUMN SALE",
+    },
+
+    {
+        id: 3,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+        title: "WINTER SALE",
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+    },
+
+    {
+        id: 2,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+    },
+
+    {
+        id: 3,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+    },
+
+    {
+        id: 4,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+    },
+
+    {
+        id: 5,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+    },
+
+    {
+        id: 6,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+    },
+
+    {
+        id: 7,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+    },
+
+    {
+        id: 8,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+    },
+]
