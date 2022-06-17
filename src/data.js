49d5573a8e7b47c99604d8061482f3ba6a690d28
RazRabino <100515676+RazRabino@users.noreply.github.com>
2022-06-17 19:14:00 +0300

    slider component

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2020/07/24/16/37/cactus-5434469_960_720.jpg",
+        title: "SUMMER SALE",
+        desc: "window shelf with succulent cactus pot.",
+        bg: "#E0FFFF",
+    },
+
+    {
+        id: 2,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/38/cactus-2556000_960_720.jpg",
+        title: "AUTUMN SALE",
+        desc: "window shelf with succulent cactus pot.",
+        bg: "#F0FFF0",
+    },
+
+    {
+        id: 3,
+        img: "https://cdn.pixabay.com/photo/2017/07/30/23/59/garlic-2556022_960_720.jpg",
+        title: "WINTER SALE",
+        desc: "window shelf with succulent cactus pot.",
+        bg: "#F5F5DC",
+    },
+]
