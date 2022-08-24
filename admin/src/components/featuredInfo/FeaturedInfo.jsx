b31f1356bb020aae3a1af08b086f7c327179593a
Arbel, Hadas <hadas_arbel_tp@bmc.com>
2022-08-24 21:20:46 +0300

    add admin

---
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,44 @@
+import "./featuredInfo.css";
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../redux/requestMethods";
+
+export default function FeaturedInfo() {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch {
+        throw console.error();
+      }
+    };
+    getIncome();
+  }, []);
+  console.log(income);
+
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Revanue</span>
+        <div className="featuredMoneyContainer">
+          {/* TODO change {"0"} to  {income[1].total} */}
+          <span className="featuredMoney">${"0"}</span>
+          <span className="featuredMoneyRate">
+            %{Math.floor(perc)}{" "}
+            {perc < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+    </div>
+  );
+}
